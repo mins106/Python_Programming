3d64d6b466ef57482af72ed02ffa51ff548ab865
mins106 <happyboysusu@gmail.com>
2026-09-03 15:31:54 +0900

    control_ex2.py

--- 03_control/ex2.py
+++ 03_control/ex2.py
@@ -1,4 +1,4 @@
-# 반복문: while문, for문
+# 반복문 : while문, for문
 
 # while문
 # 1 ~ 10까지 반복 출력
@@ -13,22 +13,37 @@ else:
 
 nums = [1, 3, 5, 7, 9]
 target = 2
+
+for i in nums:
+    if i == target:
+        print("찾았다")
+        break
+else:
+    print("못 찾았다")
+
+nums = [1, 3, 5, 7, 9]
+target = 2
 i = 0
+# found = False
+
 while i < len(nums):
     if nums[i] == target:
-        print("FOUND")
+        print(f"{target} found.")
+        # found = True
+        break
     i += 1
 else:
-    print("NOT FOUND")
+    print(f"{target} not found.")
 
-# 1 ~ 10까지의 합
+# if not found:
+#     print(f"{target} not found.")
+
 i = 1
 tot = 0
-
 while i <= 10:
-    i += 1
     if i % 2 == 1:
         continue
     tot += i
+    i += 1
 else:
-    print(f"합: {tot}")
+    print(f"짝수 합: {tot}")
